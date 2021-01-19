9738185698c2647f07bdfd263a0e3d6cc2b1136d
sukruozkoca <65304577+sukruozkoca@users.noreply.github.com>
2021-01-20 01:21:20 +0300

    KampOdevi

---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.ID + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Kimlik);
+        }
+
+        public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Kayıt Eklendi ! \n" + " = " + musteri.ID + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Kimlik+"\n");
+        }
+
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Kayıt Silindi ! \n" + " = " + musteri.ID + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Kimlik+"\n");
+        }
+        public void Güncelle(Musteri musteri)
+        {
+            Console.WriteLine("Kullanıcı Güncellendi ! \n" + " = " + musteri.ID + " " + musteri.Ad + " " + musteri.Soyad + " " + musteri.Kimlik+"\n");
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+
+        
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.ID = 1;
+            musteri1.Ad = "Şükrü";
+            musteri1.Soyad = "Özkoca";
+            musteri1.Kimlik =12345678911;
+         
+            Musteri musteri2 = new Musteri();
+            musteri2.ID = 2;
+            musteri2.Ad = "Berkay";
+            musteri2.Soyad = "Çavuşoğlu";
+            musteri2.Kimlik = 12345678912;
+
+            Musteri musteri3 = new Musteri();
+            musteri3.ID = 3;
+            musteri3.Ad = "Mehmet";
+            musteri3.Soyad = "Uysal";
+            musteri3.Kimlik = 42424242424;
+
+            MusteriManager musteriManager = new MusteriManager();
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+            foreach(var musteri in musteriler)
+            {
+                musteriManager.Listele(musteri);
+            }
+            
+            musteriManager.Ekle(musteri1);
+            musteriManager.Sil(musteri2);
+            musteriManager.Güncelle(musteri3);
+
+            Console.ReadLine();
+
+            
+
+
+
+
+        }
+       
+    }
+}
